ae147d4ecd3bef783bcd0f89de15beebc5c3748a
Sympli250 <an@symplissime.fr>
2025-08-22 21:33:33 +0200

    chore: add symplissime style

--- conseiller-rgpd.php
+++ conseiller-rgpd.php
@@ -105,6 +105,7 @@ if (isset($_POST['action']) && $_POST['action'] === 'chat') {
     <title>Conseiller RGPD IA v<?php echo htmlspecialchars($APP_VERSION); ?> - Powered by Symplissime AI</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500&family=Roboto:wght@300;400;500;700&family=Lato:wght@300;400;700&family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="conseiller-rgpd.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css">
 </head>
 <body>
